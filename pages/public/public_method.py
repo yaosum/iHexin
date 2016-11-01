7902b8a19eb170a48fad914228b7ad7b2e9e931a
tianmaotao <tianmaotao@myhexin.com>
2016-11-01 11:01:04 +0800

    public的一些改动

--- pages/public/public_method.py
+++ pages/public/public_method.py
@@ -3,9 +3,13 @@
 
 from page_object.appium_page_objects import PageObject
 
-class Public_method(PageObject):
+class PublicMethod(PageObject):
+    """
+    该类放一些公共通用的方法,命名规则:public_***
+    """
 
-    def hx_tap_element(self, element):
+    #处理某些元素点击不了,按其坐标点击
+    def public_tap_element(self, element):
         """
         Custom method. Tap element by tapping it's coordiante
         :param element: Element instance. Should have location and size attribute
@@ -14,3 +18,13 @@ class Public_method(PageObject):
         location = element.location
         el_size = element.size
         return self.w.tap([(el_size['width'] / 2 + location['x'], el_size['height'] / 2 + location['y'],)])
+
+    #长按某个元素
+    def public_longPress(self, element):
+        location = element.location
+        el_size = element.size
+
+        x = el_size['width'] / 2.0 + location['x']
+        y = el_size['height'] / 2.0 + location['y']
+
+        return self.w.tap([(x, y)], duration=2)
